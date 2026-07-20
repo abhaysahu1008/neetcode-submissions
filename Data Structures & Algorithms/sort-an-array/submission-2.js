e3ab2b1b6ca6412b0b94a63e7435a2e87745e991
abhaysahu1008 <157799359+abhaysahu1008@users.noreply.github.com>
2026-07-20 14:55:07 +0530

    Add: sort-an-array - submission-2

---
+++ Data Structures & Algorithms/sort-an-array/submission-2.js
@@ -0,0 +1,50 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    sortArray(nums) {
+        return this.mergeSort(nums);
+    }
+
+    mergeSort(arr) {
+        if (arr.length <= 1) {
+            return arr;
+        }
+
+        const mid = Math.floor(arr.length / 2);
+
+        const left = this.mergeSort(arr.slice(0, mid));
+        const right = this.mergeSort(arr.slice(mid));
+
+        return this.merge(left, right);
+    }
+
+    merge(left, right) {
+        let result = [];
+        let i = 0;
+        let j = 0;
+
+        while (i < left.length && j < right.length) {
+            if (left[i] < right[j]) {
+                result.push(left[i]);
+                i++;
+            } else {
+                result.push(right[j]);
+                j++;
+            }
+        }
+
+        while (i < left.length) {
+            result.push(left[i]);
+            i++;
+        }
+
+        while (j < right.length) {
+            result.push(right[j]);
+            j++;
+        }
+
+        return result;
+    }
+}
