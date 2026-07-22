23ed81b75726982c3d97b59de6b6f3cc0093d2ac
abhaysahu1008 <157799359+abhaysahu1008@users.noreply.github.com>
2026-07-22 16:01:55 +0530

    Add: valid-palindrome-ii - submission-1

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-1.js
@@ -0,0 +1,37 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    validPalindrome(s) {
+
+        function isPalindrome(left, right) {
+            while (left < right) {
+                if (s[left] !== s[right]) {
+                    return false;
+                }
+                left++;
+                right--;
+            }
+            return true;
+        }
+
+        let left = 0;
+        let right = s.length - 1;
+
+        while (left < right) {
+            if (s[left] !== s[right]) {
+                // Skip either the left or the right character
+                return (
+                    isPalindrome(left + 1, right) ||
+                    isPalindrome(left, right - 1)
+                );
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
