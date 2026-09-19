35fb799f97ef6a4ad603330d81eff89d23184279
abhaysahu1008 <157799359+abhaysahu1008@users.noreply.github.com>
2026-09-19 11:59:28 +0530

    Add: remove-duplicates-from-sorted-array - submission-0

---
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array/submission-0.js
@@ -0,0 +1,22 @@
+class Solution {
+    removeDuplicates(nums) {
+        if (nums.length === 0) return 0;
+        
+        let temp = [];
+        temp[0] = nums[0];
+        let t = 1;
+        
+        for (let i = 1; i < nums.length; i++) {
+            if (nums[i] !== nums[i - 1]) {
+                temp[t] = nums[i];
+                t++;
+            }
+        }
+        
+        for (let k = 0; k < t; k++) {
+            nums[k] = temp[k];
+        }
+        
+        return t;
+    }
+}
