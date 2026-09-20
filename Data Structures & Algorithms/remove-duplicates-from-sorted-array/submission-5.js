fa6ca5cf0190cb0aa2d077459faa761337ea28c8
abhaysahu1008 <157799359+abhaysahu1008@users.noreply.github.com>
2026-09-20 13:14:32 +0530

    Add: remove-duplicates-from-sorted-array - submission-5

---
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array/submission-5.js
@@ -0,0 +1,13 @@
+class Solution {
+    removeDuplicates(nums) {
+        let j=0;
+        
+        for(let i=1;i<nums.length;i++){
+            if(nums[i]!==nums[j]){
+                j++;
+                nums[j]=nums[i];
+            }
+        }
+        return j+1;
+    }
+}
