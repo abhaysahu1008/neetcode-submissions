f4e7c6e16854916c32a65ee9bce60d14b7c7ea42
abhaysahu1008 <157799359+abhaysahu1008@users.noreply.github.com>
2026-09-25 23:21:53 +0530

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums, target) {let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        // Prevents overflow compared to Math.floor((left + right) / 2)
+        const mid = left + Math.floor((right - left) / 2);
+
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return -1;}
+}
